96994a9fe4b21dafa4f239a5b7c7562e7ae1f0aa
Emile De Wilde <synaptic4u@gmail.com>
2025-04-04 11:22:49 +0200

    Major mods -> should pull the other repos from packagist.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array(
+    'root' => array(
+        'name' => 'synaptic4u/logging',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '500aeff54703453a7ff2583ccbd8175489fbf7b3',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'synaptic4u/db' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a0297ba9a46bf77692fdae11f395b6de0471601c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../synaptic4u/db',
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+        'synaptic4u/logging' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '500aeff54703453a7ff2583ccbd8175489fbf7b3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
---
+++ vendor/synaptic4u/db/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'synaptic4u/db',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6459171153562bf9049591cb09001f8ebddd5110',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'synaptic4u/db' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6459171153562bf9049591cb09001f8ebddd5110',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
